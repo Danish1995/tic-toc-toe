df3179244dfb1e9d8b7f1875c6790adc9a6ccc04
Danish mahmood <d.m.javid95@gmail.com>
2024-03-29 02:04:09 +0100

    Unnecessary state removed

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -19,28 +19,25 @@ export default function GameBoard({ squareClick, turns }) {
   // effects and enables proper handling of component updates.
 
   return (
-    <>
-      <h1>sss</h1>
-      <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button onClick={() => squareClick(rowIndex, colIndex)}>
-                    {/* When you write onClick={handleSelectSquare(rowIndex, colIndex)}, you're not passing a function reference;
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => squareClick(rowIndex, colIndex)}>
+                  {/* When you write onClick={handleSelectSquare(rowIndex, colIndex)}, you're not passing a function reference;
                    instead, you're invoking the function immediately, and the onClick handler receives the result of that 
                    function call, which is undefined in this case. To fix this, you need to pass a function reference to
                     onClick. You can achieve this by using an arrow function or the bind method. Here's how you can modify 
                     your button component: */}
-                    {playerSymbol}
-                  </button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    </>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
   );
 }
