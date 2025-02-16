892d3bbdaae5600b17e27cbd35389e469bd20f2c
Danish <d.m.javid95@gmail.com>
2025-02-16 16:28:07 +0100

    import update

--- src/components/ExtraComponent.js
+++ src/components/ExtraComponent.js
@@ -3,4 +3,4 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
-import GameOver from "./components/GameOver";
+import GameOver from "./components/Log";
